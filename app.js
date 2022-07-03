f43def8b24e7c232047ecadbb58e19d83f8832c8
Puffafissh <tv2290@columbia.edu>
2022-07-03 01:52:57 -0700

    calculation of timedp

---
+++ app.js
@@ -0,0 +1,22 @@
+let  availableDate = [   
+        day,
+        month,
+        year,  
+];
+console.log(availableDate.length)
+
+let  availableTime = [];
+console.log(availableTime.length)
+
+let  unavailableDate = [];
+console.log(unavailableDate.length)
+
+let  unavailableTime = [];
+console.log(unavailableTime.length)
+
+availableDate.reduce((accumulator, {day, month}))
+
+availableTime.reduce((accumulator, {hours, minutes}) => {
+    return accumulator + (min/60 + hours)
+}, 0)
+
